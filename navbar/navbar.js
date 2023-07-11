2129f12168c537511e571e6633459d8d227c7209
ANIKET PATRA <aniket.patra12345@gmail.com>
2023-07-11 21:49:08 +0530

    navbar 2nd commit

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,51 +1,8 @@
-// Selecting the sidebar and buttons
-const sidebar = document.querySelector(".sidebar");
-const sidebarOpenBtn = document.querySelector("#sidebar-open");
-const sidebarCloseBtn = document.querySelector("#sidebar-close");
-const sidebarLockBtn = document.querySelector("#lock-icon");
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
 
-// Function to toggle the lock state of the sidebar
-const toggleLock = () => {
-    sidebar.classList.toggle("locked");
-    // If the sidebar is not locked
-    if (!sidebar.classList.contains("locked")) {
-        sidebar.classList.add("hoverable");
-        sidebarLockBtn.classList.replace("bx-lock-alt", "bx-lock-open-alt");
-    } else {
-        sidebar.classList.remove("hoverable");
-        sidebarLockBtn.classList.replace("bx-lock-open-alt", "bx-lock-alt");
-    }
+const toggleNavbar = () => {
+    nav_header.classList.toggle("active");
 };
 
-// Function to hide the sidebar when the mouse leaves
-const hideSidebar = () => {
-    if (sidebar.classList.contains("hoverable")) {
-        sidebar.classList.add("close");
-    }
-};
-
-// Function to show the sidebar when the mouse enter
-const showSidebar = () => {
-    if (sidebar.classList.contains("hoverable")) {
-        sidebar.classList.remove("close");
-    }
-};
-
-// Function to show and hide the sidebar
-const toggleSidebar = () => {
-    sidebar.classList.toggle("close");
-};
-
-// If the window width is less than 800px, close the sidebar and remove hoverability and lock
-if (window.innerWidth < 800) {
-    sidebar.classList.add("close");
-    sidebar.classList.remove("locked");
-    sidebar.classList.remove("hoverable");
-}
-
-// Adding event listeners to buttons and sidebar for the corresponding actions
-sidebarLockBtn.addEventListener("click", toggleLock);
-sidebar.addEventListener("mouseleave", hideSidebar);
-sidebar.addEventListener("mouseenter", showSidebar);
-sidebarOpenBtn.addEventListener("click", toggleSidebar);
-sidebarCloseBtn.addEventListener("click", toggleSidebar);
+mobile_nav.addEventListener("click", () => toggleNavbar());
